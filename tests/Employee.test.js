70426b0fe075ade8b0da68d9808da316b54ec578
Duran11 <antonio11duran@gmail.com>
2022-10-25 11:42:36 -0600

    added employee test

---
+++ tests/Employee.test.js
@@ -0,0 +1,25 @@
+const Employee = require("../lib/Employee");
+
+describe('getName', () => {
+    it('should return name of employee', () => {
+        expect(this.name);
+    })
+});
+
+describe('getId', () => {
+    it('should return id of employee', () => {
+        expect(this.id);
+    })
+});
+
+describe('getEmail', () => {
+    it('should return email of employee', () => {
+        expect(this.email);
+    })
+});
+
+describe('getRole', () => {
+    it('should return role of employee', () => {
+        expect('Employee');
+    })
+});
